3c3c76e55e8c35857581763147e086ab3153ca46
rosariohg <rhuancagonza@gmail.com>
2017-08-28 22:52:16 -0500

    Funciones

---
+++ Funciones/Funciones/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Funciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFunciones = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtVal2 = new System.Windows.Forms.TextBox();
+            this.txtVal1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Funciones";
+            // 
+            // cmbFunciones
+            // 
+            this.cmbFunciones.FormattingEnabled = true;
+            this.cmbFunciones.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División",
+            "Convertir a Celsius",
+            "Convertir a Farenheit"});
+            this.cmbFunciones.Location = new System.Drawing.Point(105, 21);
+            this.cmbFunciones.Name = "cmbFunciones";
+            this.cmbFunciones.Size = new System.Drawing.Size(219, 21);
+            this.cmbFunciones.TabIndex = 1;
+            this.cmbFunciones.SelectedIndexChanged += new System.EventHandler(this.cmbFunciones_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtVal2);
+            this.groupBox1.Controls.Add(this.txtVal1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 117);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Valores";
+            // 
+            // txtVal2
+            // 
+            this.txtVal2.Enabled = false;
+            this.txtVal2.Location = new System.Drawing.Point(77, 74);
+            this.txtVal2.Name = "txtVal2";
+            this.txtVal2.Size = new System.Drawing.Size(100, 20);
+            this.txtVal2.TabIndex = 7;
+            // 
+            // txtVal1
+            // 
+            this.txtVal1.Enabled = false;
+            this.txtVal1.Location = new System.Drawing.Point(77, 32);
+            this.txtVal1.Name = "txtVal1";
+            this.txtVal1.Size = new System.Drawing.Size(100, 20);
+            this.txtVal1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor 1";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCalcular.Location = new System.Drawing.Point(354, 97);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(134, 62);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Resultado";
+            // 
+            // txtRes
+            // 
+            this.txtRes.Enabled = false;
+            this.txtRes.Location = new System.Drawing.Point(105, 212);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(100, 20);
+            this.txtRes.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 276);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbFunciones);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FUNCIONES";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFunciones;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtVal2;
+        private System.Windows.Forms.TextBox txtVal1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRes;
+    }
+}
+
---
+++ Funciones/Funciones/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funciones
+{
+    public partial class Form1 : Form
+    {
+        string func;
+        double res;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+        static double Resta(double a, double b)
+        {
+            return a - b;
+        }
+        static double Multiplicacion(double a, double b)
+        {
+            return a * b;
+        }
+        static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+        static void Primos()
+        {
+            int cont = 0;
+            for (int i = 2; i <= 30; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        cont = cont + 1;
+                    }
+                }
+
+                if (cont <= 2)
+                {
+                    //Console.WriteLine("{0} es número primo ", i);
+                    MessageBox.Show(i + "\n");
+                }
+                cont = 0;
+            }
+        }
+        static double Celsius(double f)
+        {
+            return (5 * (f - 32) / 9);
+        }
+        static double Farenheit(double c)
+        {
+            return ((9 * c) / 5) + 32;
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            double val1 = 0; 
+            double val2 = 0;
+            func = cmbFunciones.Text;
+            
+            switch (func)
+            {
+                case "Suma":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    val2 = Convert.ToInt32(txtVal2.Text);
+                    res = Suma(val1, val2);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                case "Resta":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    val2 = Convert.ToInt32(txtVal2.Text);
+                    res = Resta(val1, val2);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                case "Multiplicación":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    val2 = Convert.ToInt32(txtVal2.Text);
+                    res = Multiplicacion(val1, val2);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                case "División":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    val2 = Convert.ToInt32(txtVal2.Text);
+                    res = Division(val1, val2);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                case "Convertir a Celsius":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    res = Celsius(val1);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                case "Convertir a Farenheit":
+                    val1 = Convert.ToInt32(txtVal1.Text);
+                    res = Farenheit(val1);
+                    txtRes.Text = Convert.ToString(res);
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+            
+        }
+
+        public void limpiar() {
+            txtVal1.Clear();
+            txtVal2.Clear();
+            txtRes.Clear();
+        }
+        private void cmbFunciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string funcion = cmbFunciones.SelectedItem.ToString();
+            switch (funcion)
+            {
+                case "Suma":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = true;
+                    break;
+                case "Resta":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = true;
+                    break;
+                case "Multiplicación":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = true;
+                    break;
+                case "División":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = true;
+                    break;
+                case "Convertir a Celsius":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = false;
+                    break;
+                case "Convertir a Farenheit":
+                    limpiar();
+                    txtVal1.Enabled = true;
+                    txtVal2.Enabled = false;
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+        }
+    }
+}
