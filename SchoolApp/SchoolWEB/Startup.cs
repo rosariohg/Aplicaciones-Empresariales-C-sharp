4323d8aa077ae1a18ea2eef100ade9ff61cb3e50
rosariohg <rosariohg>
2017-10-26 17:19:55 -0500

    ADO .NET

---
+++ SchoolApp/SchoolWEB/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolWEB.Startup))]
+namespace SchoolWEB
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
